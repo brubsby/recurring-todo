e971ff1d79313c0f46274c73849cdc30d2e0e93f
Tyler Busby <TylerBusby333@gmail.com>
2017-01-12 23:58:45 -0600

    Add time based refreshes

--- refresh.py
+++ refresh.py
@@ -2,6 +2,47 @@ import ConfigParser
 import xmind
 import pprint
 import ast
+import datetime
+
+
+def shouldRefresh(recurrence, lastUpdatedDate, todayDate):
+	if recurrence['type'] == 'daily':
+		return todayDate != lastUpdatedDate
+	elif recurrence['type'] == 'weekly':
+		if todayDate == lastUpdatedDate:
+			return False
+		tempDate = todayDate - datetime.timedelta(1)
+		while lastUpdatedDate < tempDate:
+			if tempDate == lastUpdatedDate:
+				return False
+			elif tempDate.weekday() == recurrence['day']:
+				return True
+			tempDate = tempDate - datetime.timedelta(1)
+	elif recurrence['type'] == 'monthly':
+		lastResetDate = todayDate.replace(day=1)
+		if todayDate == lastUpdatedDate:
+			return False
+		return lastResetDate > lastUpdatedDate
+	return False
+
+
+def stringToDate(string):
+	return datetime.datetime.strptime(string, "%Y-%m-%d").date()
+
+
+def writeConfigFromXMind(subtopics, config):
+	for subtopic in subtopics:
+		config.add_section(subtopic.getTitle())
+		tasklist = []
+		for subsubtopic in subtopic.getSubTopics():
+			tasklist.append(subsubtopic.getTitle())
+		config.set(subtopic.getTitle(), 'tasks', str(tasklist))
+
+
+def saveConfig(config):
+	with open(configpath, 'w') as configfile:
+		config.write(configfile)
+
 
 configpath = "config.ini"
 config = ConfigParser.SafeConfigParser()
@@ -24,44 +65,36 @@ if not subTopics:
 subTopicTitles = list(subTopic.getTitle().encode("utf-8") for subTopic in subTopics)
 
 configSections = config.sections()
-
-pprint.pprint(configSections)
-pprint.pprint(subTopicTitles)
+todayDate = datetime.datetime.utcnow().date()
+rootTopicNotes = rootTopic.getNotes()
+if rootTopicNotes:
+	rootTopicNotesText = rootTopicNotes.getContent().encode("utf-8")
+	lastUpdatedDate = stringToDate(rootTopicNotesText)
+else:
+	lastUpdatedDate = datetime.date.min
+rootTopic.setPlainNotes(str(todayDate))
 
 for configSection in configSections:
-	# add subtopic from config if it is not in the sheet
-	if configSection not in subTopicTitles:
-		subTopic = xmind.core.topic.TopicElement()
-		subTopic.setTitle(configSection)
-		rootTopic.addSubTopic(subTopic)
-	else:
-		# if it is in the sheet, get it
-		subTopic = next(subTopic for subTopic in subTopics if subTopic.getTitle() == configSection)
-	tasks = ast.literal_eval(config.get(configSection, "tasks"))
-	# get existing task topics
-	existingTaskTopics = subTopic.getSubTopics()
-	# if empty, replace with empty list
-	if not existingTaskTopics:
-		existingTaskTopics = []
-	existingTaskTopicTitles = list(existingTaskTopic.getTitle().encode("utf-8") for existingTaskTopic in existingTaskTopics)
-	for task in tasks:
-		if task not in existingTaskTopicTitles:
-			newTaskTopic = xmind.core.topic.TopicElement()
-			newTaskTopic.setTitle(task)
-			subTopic.addSubTopic(newTaskTopic)
+	recurrence = ast.literal_eval(config.get(configSection, "recurrence"))
+	if shouldRefresh(recurrence, lastUpdatedDate, todayDate):
+		# add subtopic from config if it is not in the sheet
+		if configSection not in subTopicTitles:
+			subTopic = xmind.core.topic.TopicElement()
+			subTopic.setTitle(configSection)
+			rootTopic.addSubTopic(subTopic)
+		else:
+			# if it is in the sheet, get it
+			subTopic = next(subTopic for subTopic in subTopics if subTopic.getTitle() == configSection)
+		tasks = ast.literal_eval(config.get(configSection, "tasks"))
+		# get existing task topics
+		existingTaskTopics = subTopic.getSubTopics()
+		if not existingTaskTopics:
+			existingTaskTopics = []
+		existingTaskTopicTitles = list(existingTaskTopic.getTitle().encode("utf-8") for existingTaskTopic in existingTaskTopics)
+		for task in tasks:
+			if task not in existingTaskTopicTitles:
+				newTaskTopic = xmind.core.topic.TopicElement()
+				newTaskTopic.setTitle(task)
+				subTopic.addSubTopic(newTaskTopic)
 
 xmind.save(workbook, filepath)
-
-
-def writeConfigFromXMind(subtopics, config):
-	for subtopic in subtopics:
-		config.add_section(subtopic.getTitle())
-		tasklist = []
-		for subsubtopic in subtopic.getSubTopics():
-			tasklist.append(subsubtopic.getTitle())
-		config.set(subtopic.getTitle(), 'tasks', str(tasklist))
-
-
-def saveConfig(config):
-	with open(configpath, 'w') as configfile:
-		config.write(configfile)
